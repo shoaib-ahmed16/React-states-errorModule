754f3ee053ebaa7996acfce4ddbc0a6a7a35e796
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2023-04-04 22:54:59 +0530

    addding protal code

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,14 +1,13 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Card from './Card'
 import Button from './Button'
 import classes from './ErrorModel.module.css'
-import Wrapper from '../Helper/Wrapper'
-const ErrorModal =(props)=>{
-
-  return(
-    <>
-      <div className={classes.backdrop} onClick={props.onConfirm}></div>
-    <Card className={classes.model}>
+const Backdrop =(props)=>{
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>
+}
+const ModelOverlay =(props)=>{
+  return <Card className={classes.model}>
       <header className={classes.header}>
         <h2>{props.errorTitle}</h2>
       </header>
@@ -19,6 +18,12 @@ const ErrorModal =(props)=>{
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
+}
+const ErrorModal =(props)=>{
+  return(
+    <>
+    {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>,document.getElementById("backdrop-root"))}
+    {ReactDOM.createPortal(<ModelOverlay errorTitle={props.errorTitle} errorMessage={props.errorMessage} onConfirm={props.onConfirm}/>,document.getElementById("overlay-root"))}
     </>
   )
 }
